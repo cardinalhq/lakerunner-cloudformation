f2e7e8b22186ce922674d1699dd42856d4e99175
Michael Graff <explorer@flame.org>
2025-08-13 10:51:44 -0500

    licenses

--- test/lakerunner-cloudformation.test.ts
+++ test/lakerunner-cloudformation.test.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 CardinalHQ, Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as LakerunnerCloudformation from '../lib/lakerunner-cloudformation-stack';
